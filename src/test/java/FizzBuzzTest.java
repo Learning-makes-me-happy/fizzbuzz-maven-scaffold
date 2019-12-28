1c55589302fdd39fa026f758097b6ad33ab858c3
THTF <l100673929@outlook.com>
2019-12-28 12:39:24 +0800

    'refactor'

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -1,12 +1,34 @@
-import org.fest.assertions.core.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
 
 import static junit.framework.Assert.assertEquals;
-
+@RunWith(Parameterized.class)
 public class FizzBuzzTest {
+    private String excepted;
+    private int number;
+
+    public FizzBuzzTest(String excepted, int number) {
+        this.excepted = excepted;
+        this.number = number;
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+         return Arrays.asList(new Object[][]{
+                 {"1",1},
+                 {"Fizz",3},
+                 {"Buzz",5},
+                 {"FizzBuzz",15}
+         });
+    }
+
     @Test
     public void test(){
-        int number = 3;
-        assertEquals("Fizz", FizzBuzz.of(number));
+
+        assertEquals(excepted, FizzBuzz.of(number));
     }
 }
